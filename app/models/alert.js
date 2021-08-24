0c4209d7d615a3b242cee80e70a18c052b2d5c88
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2021-08-23 23:05:25 -0300

    feat: add immutable prop to mongoose schema, avoiding change when update

--- app/models/alert.js
+++ app/models/alert.js
@@ -4,7 +4,7 @@ const notificationSchema = new mongoose.Schema({
   email: { type: String, required: true },
   term: { type: String, required: true },
   frequency: { type: Number, required: true },
-  created: { type: Date, default: Date.now },
+  created: { type: Date, default: Date.now, immutable: true },
   updated: { type: Date, default: Date.now },
 });
 
